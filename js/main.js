12990d60ab2da7eaa7078aeedc21388b37b5fb84
skoff.ua <skoff.ua@gmail.com>
2015-01-24 22:47:38 +0200

    added menu jQuery

--- js/main.js
+++ js/main.js
@@ -3,17 +3,20 @@
 /*-----------------------SideBar----------------------*/
 $(document).ready(function(){
   $(".navigation span").click(function(){
-    $(this).siblings(".toggle-menu").slideToggle(100);
+
+  	$(".navigation span").toggle("slide", {direction: "left"}, 500);
+    $(".toggle-menu").toggle("slide", {direction: "right"}, 500);
+
+  	//$(".menu").css({ "border": "none"});
+
   });
  	$(".filter").click(function () {
  		$(".filter-menu").slideToggle(100);
  	});
-});
 /*-----------------------Filter----------------------*/
 var mas = [];
 var a = "";
 
-$(document).ready(function(){
   $(".filter-tag").on("click", function() {
  		a = $(this).text().slice(1);
 		mas = $(".filter-search li").text().split("#").slice(1);
