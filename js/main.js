03131fd4f20f461335935bb115512c13da72fb87
skoff.ua <skoff.ua@gmail.com>
2015-02-10 14:36:40 +0200

    delete useless structure, styles, js.

--- js/main.js
+++ js/main.js
@@ -2,32 +2,16 @@
 
 /*-----------------------SideBar----------------------*/
 $(document).ready(function(){
-	var lol;
+	var menu;
   $(".navigation span").click(function(){
-  	lol = $(this).next();
+  	menu = $(this).next();
 
-  	if (lol.text() != ""){
+  	if (menu.text() != ""){
 	    $(this).next().show().animate({left: "-=288px"}, 500);
 	    $(".navigation span").animate({left: "-=288px"}, 500);
 		}
   });
  	$(".filter").click(function () {
  		$(".filter-menu").slideToggle(100);
- 	});
-/*-----------------------Filter----------------------*/
-var mas = [];
-var a = "";
-
-  $(".filter-tag").on("click", function() {
- 		a = $(this).text().slice(1);
-		mas = $(".filter-search li").text().split("#").slice(1);
-
-		if (mas.indexOf(a) == -1){
-			$(".filter-search div ul").prepend("<li>" + this.textContent + "</li>");
-		}
-		$(".filter-search li").on("click", function() {
-	    $(this).remove();
-		});
-	});
-	
+ 	});	
 });
