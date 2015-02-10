d74dbba28dd13da83064bd03fb6e081cb400104b
skoff.ua <skoff.ua@gmail.com>
2015-02-10 20:44:10 +0200

    added js for sidebar

--- js/main.js
+++ js/main.js
@@ -14,4 +14,27 @@ $(document).ready(function(){
  	$(".filter").click(function () {
  		$(".filter-menu").slideToggle(100);
  	});	
+		
+
+/*----------------icon-menu-----------------------------*/
+	var textId = "";
+	$(".icon-menu li").click(function(){
+		textId = $(this).children("span").attr("id");
+		$(this).siblings().removeClass("active");
+		$(this).addClass("active");
+		if (textId === "menu"){
+			$(".icon-menu").siblings().addClass("hidden");
+			$(".menu").removeClass("hidden");
+		} else if (textId === "login") {
+				$(".icon-menu").siblings().addClass("hidden");
+				$(".login").removeClass("hidden");
+		} else if (textId === "tags") {
+				$(".icon-menu").siblings().addClass("hidden");
+				$(".tags").removeClass("hidden");
+		} else if (textId === "search") {
+				$(".icon-menu").siblings().addClass("hidden");
+				$(".search").removeClass("hidden");
+		}
+	});
 });
+
