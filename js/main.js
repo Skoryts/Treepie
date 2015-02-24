b939e913bb6e1943848715f26c9e3de81d148f2a
skoff.ua <skoff.ua@gmail.com>
2015-02-24 22:02:29 +0200

    delete useless span, and added action form

--- js/main.js
+++ js/main.js
@@ -1,116 +1,56 @@
 
 
 $(document).ready(function(){
-	stylesheetToggle();
-  $(window).resize(stylesheetToggle);
+	/*----------------icon-menu-----------------------------*/		
+	  $(".icon-menu li").click(function(){
+	  	var listIndex = $(this).index();
+	  	var masMenu = $(".icon-menu").siblings();
+
+	  	/*----------close-open----------------*/	
+	  	if (masMenu[listIndex].classList.contains("hidden") == false){
+	  		masMenu.addClass("hidden");
+	  		$(".icon-menu li").removeClass("active");
+	  		return false;
+	  	}
+	  	if ($(".sidebar").css("height") == "96px") {	  		
+		  	$(".navigation li div").css("right", "auto");
+				$(".toggle-menu").hide().css("right", "-288px");
+	  	} else {
+	  		$(".navigation li div").css("left", "auto");
+				$(".toggle-menu").hide().css("left", "288px");
+	  	}
+	  	masMenu.addClass("hidden");
+	  	masMenu[listIndex].classList.remove("hidden");
+	  	$(".icon-menu li").removeClass("active");
+	  	$(this).addClass("active");
+	  	
+	  });
+
+
+	
+	if ($(".sidebar").css("height") == "96px") {
+		var menuActive = "";
+		/*-----------------------Toggle-slim-menu----------------------*/
+	  $(".navigation li div").click(function(){
+	  	if ($(this).next().text() != ""){
+	  		$(".navigation li div").animate({right: "+=288px"}, 300);
+	  		$(this).next().show().animate({right: "+=288px"}, 300);
+	  	}
+	  });
+	} else {
+		/*-----------------------Toggle-menu----------------------*/
+	  $(".navigation li div").click(function(){
+	  	if ($(this).next().text() != ""){
+	  		$(".navigation li div").animate({left: "-=288px"}, 300);
+	  		$(this).next().show().animate({left: "-=288px"}, 300);
+	  	}
+	  });
+	}
 
 });
 
-function stylesheetToggle() {
-/*----------------Style >= 1400px-----------------------------*/
-	if (window.innerWidth >= 1400) {
-		$(".icon-menu").siblings().addClass("hidden");
-		$(".menu").removeClass("hidden");
-		$(".icon-menu").children().removeClass("active");
-		$(".icon-menu li:first-child").addClass("active");
 
-		/*-----------------------SideBar----------------------*/
-	  $(".navigation span").click(function(){
-	  	var menu;
-	  	menu = $(this).next();
-	  	if (menu.text() != ""){
-		    $(this).next().show().animate({left: "-=288px"}, 500);
-		    $(".navigation span").animate({left: "-=288px"}, 500);
-			}
-	  });
-	  /*----------------icon-menu-----------------------------*/
-		var textId = "";
-		$(".icon-menu li").click(function(){
-			textId = $(this).children("span").attr("id");
-			$(this).siblings().removeClass("active");
-			$(this).addClass("active");
-			if (textId === "menu"){
-				$(".icon-menu").siblings().addClass("hidden");
-				$(".menu").removeClass("hidden");
-			} else if (textId === "login") {
-					$(".icon-menu").siblings().addClass("hidden");
-					$(".login").removeClass("hidden");
-			} else if (textId === "tags") {
-					$(".icon-menu").siblings().addClass("hidden");
-					$(".tags").removeClass("hidden");
-			} else if (textId === "search") {
-					$(".icon-menu").siblings().addClass("hidden");
-					$(".search").removeClass("hidden");
-			}
-		});
-	}
-/*----------------Style <= 1399px-----------------------------*/
-	if (window.innerWidth <= 1399) {
-		var menuActive = "";
-		$(".icon-menu").siblings().addClass("hidden");
-		$(".icon-menu").children().removeClass("active");
-		/*-----------------------SideBar----------------------*/
-	  $(".navigation span").click(function(){
-	  	var menu;
-	  	menu = $(this).next();
-	  	if (menu.text() != ""){
-		    $(this).next().show().animate({right: "+=288px"}, 500);
-		    $(".navigation span").animate({right: "+=288px"}, 500);
-			}
-	  });	
-	  /*----------------icon-menu-----------------------------*/
-		$(".icon-menu li").click(function(){
-			var textId = "";
-			textId = $(this).children("span").attr("id");
-			$(this).siblings().removeClass("active");
-			$(this).addClass("active");
-			switch (textId) {
-				case "menu":	
-					if (menuActive == "menu block"){
-						$(".icon-menu").siblings().addClass("hidden");
-						menuActive = ""
-						break;
-					} else {
-							$(".icon-menu").siblings().addClass("hidden");
-							$(".menu").removeClass("hidden");
-							menuActive = "menu " + $(".menu").css("display");
-							break;
-						}
-				case "login":
-					if (menuActive == "login block"){
-						$(".icon-menu").siblings().addClass("hidden");
-						menuActive = ""
-						break;
-					} else {
-						$(".icon-menu").siblings().addClass("hidden");
-						$(".login").removeClass("hidden");
-						menuActive = "login " + $(".login").css("display");
-						break;
-					}
-				case "tags":
-					if (menuActive == "tags block"){
-						$(".icon-menu").siblings().addClass("hidden");
-						menuActive = ""
-						break;
-					} else {
-						$(".icon-menu").siblings().addClass("hidden");
-						$(".tags").removeClass("hidden");
-						menuActive = "tags " + $(".tags").css("display");
-						break;
-					}
-				case "search":
-					if (menuActive == "search block"){
-						$(".icon-menu").siblings().addClass("hidden");
-						menuActive = ""
-						break;
-					} else {
-						$(".icon-menu").siblings().addClass("hidden");
-						$(".search").removeClass("hidden");
-						menuActive = "search " + $(".search").css("display");
-						break;
-					}
-			} 	
-		});
-	}
-}
+	
+	  
+
 
