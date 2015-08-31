85e65781c9d179d468621a08def46c649fd6331d
tooleks <tooleks@gmail.com>
2015-09-01 00:41:56 +0300

    removed unused alert from the search.js file

--- web/js/search.js
+++ web/js/search.js
@@ -2,7 +2,7 @@ $("#search-form").submit(function () {
     $.ajax({
         url: $(this).prop('action'),
         data: $(this).serialize(),
-        success: function (data) {alert(data);
+        success: function (data) {
             $('main').html(data);
             $('html').animate({ scrollTop: 0 }, 'fast');
             //todo: add action to close the sidebar  [@tooleks]
